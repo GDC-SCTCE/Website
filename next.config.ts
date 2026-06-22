17eda9bcd3eed745e14c1252820158330b16d894
Akhileswaran-K-R <akhilarathi956@gmail.com>
2026-06-23 04:15:49 +0530

    allowed netlify

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      allowedOrigins: ["localhost:3000", "*.devtunnels.ms"],
+      allowedOrigins: ["localhost:3000", "*.devtunnels.ms", "*.netlify.app"],
     },
   },
   images: {
