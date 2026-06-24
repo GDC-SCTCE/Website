8722d81735037f6f5bec9ddc82827a2ac7526061
Akhileswaran-K-R <akhilarathi956@gmail.com>
2026-06-24 09:51:57 +0530

    made navbar sticky in phone

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,10 +4,10 @@ import FloatingHUD from "@/components/FloatingHUD";
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-[#131314] flex flex-col relative overflow-x-hidden">
+    <div className="min-h-screen bg-[#131314] flex flex-col relative">
       <Navbar />
       {/* ── PAGE CONTENT ── */}
-      <main className="flex-1">
+      <main className="flex-1 overflow-x-hidden">
         {children}
       </main>
       <FloatingHUD />
